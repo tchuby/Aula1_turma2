01fef45ab123b1a416fc5663fb2ea556aa5117d9
danieltchuby@outlook.com <danieltchuby@outlook.com>
2019-09-18 17:29:00 -0300

    Aula 18-09 Final

---
+++ MinhaPrimeiraClass/MinhaSegundaClasse/Model/Frutas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaSegundaClasse.Model
+{
+    class Frutas
+    {
+
+        public string Nome { get; set; }
+
+        public int Quantidade { get; set; }
+
+
+    }
+}
---
+++ MinhaPrimeiraClass/MinhaSegundaClasse/Program.cs
@@ -0,0 +1,37 @@
+﻿using MinhaSegundaClasse.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaSegundaClasse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var minhaCestaDeFrutas = new List<Frutas>();
+
+            minhaCestaDeFrutas.Add(new Frutas()
+            {
+                Nome = "Banana",
+                Quantidade = 5
+
+            });
+
+            minhaCestaDeFrutas.Add(new Frutas()
+            {
+                Nome = "Banana",
+                Quantidade = 2
+
+            });
+                                   
+            minhaCestaDeFrutas.ForEach(i => Console.WriteLine($"Fruta > Fruta:{i.Nome} Quantidade:{i.Quantidade}"));
+
+            Console.ReadKey();
+
+        }
+    }
+}
