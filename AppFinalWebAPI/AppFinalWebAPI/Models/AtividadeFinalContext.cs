40fc117b9c5bca8e57171a5eb52b888bf3fd702b
danieltchuby@outlook.com <danieltchuby@outlook.com>
2019-11-05 18:01:25 -0200

    Aula 05-11

---
+++ AppFinalWebAPI/AppFinalWebAPI/Models/AtividadeFinalContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace AppFinalWebAPI.Models
+{
+    public partial class AtividadeFinalContext : DbContext
+    {
+        public AtividadeFinalContext()
+        {
+        }
+
+        public AtividadeFinalContext(DbContextOptions<AtividadeFinalContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Usuario> Usuario { get; set; }
+
+       
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.Property(e => e.Email).HasMaxLength(50);
+
+                entity.Property(e => e.Login).HasMaxLength(15);
+
+                entity.Property(e => e.Nome)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Senha).HasMaxLength(15);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ AppFinalWebAPI/AppFinalWebAPI/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppFinalWebAPI.Models
+{
+    public partial class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
